848397c80f707f5367e7d1eccfbb663c10421e8d
WCroW1997 <wcrow1997@gmail.com>
2021-12-29 08:26:02 +0200

    add todo.js

--- ToDo/main.js
+++ ToDo/main.js
@@ -7,10 +7,10 @@ let number = 1
 let deleteBtn = document.getElementsByClassName('delete__btn')
 let deleteAllBtn = document.querySelector('.delete__all__btn')
 
-let tasks = []
+let tasks
 let trash = []
 
-console.log(deleteBtn)
+!localStorage.tasks ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'))
 
 
 createBtn.addEventListener("click", function() {
@@ -20,7 +20,12 @@ createBtn.addEventListener("click", function() {
     creator(number, todotextValue, tegValue, setDate())
     render(tasks)
     number++
+    addLocal()
+
 })
+
+
+
 
 function moveTrash (element) {
     let strs = element.id.split('+')
@@ -29,7 +34,7 @@ function moveTrash (element) {
     tasks.split(index, 1)
 }
 
-deleteBtn.addEventListener("click", moveTrash)
+// deleteBtn.addEventListener("click", moveTrash)
 
 // deleteAllBtn.addEventListener("click", function() {
 //     let list = document.getElementsByClassName('.todo__item')
@@ -45,26 +50,47 @@ const creator = (numberValue, textValue, tagValue, dateValue) => {
     }
 
     tasks.push(item)
-    console.log(tasks)
 }
 
 
 
-const render = (tasks) => {
+// const render = (tasks , index) => {
+//     todoList.innerHTML = ''
+//     tasks.forEach(element => {
+//         const liElement = document.createElement('li')
+//               liElement.className = 'todo__item'
+//               liElement.id = `todo__item`
+//               liElement.insertAdjacentHTML('afterbegin', `
+//                           <span class="number"> ${element.number}</span>
+//                           <span class="text">${element.text}</span>
+//                           <span class="tag"> ${element.tag}</span>
+//                           <span class="date">${element.date}</span>
+//                           <button onclick="deleteTask(${index})" class="delete__btn" id="delete__btn">Удалить</button> 
+//             `)
+//         todoList.append(liElement)
+//     });
+// }
+
+// const creatorElement = (tasks, index) => {
+//     let liElement.insertAdjacentHTML('afterbegin', `
+//                             <li class="todo__item">
+//                               <span class="number"> ${element.number}</span>
+//                               <span class="text">${element.text}</span>
+//                               <span class="tag"> ${element.tag}</span>
+//                               <span class="date">${element.date}</span>
+//                               <button onclick="deleteTask(${index})" class="delete__btn" id="delete__btn">Удалить</button> 
+//                             </li>
+//                 `)
+// }
+
+
+const fillList = () => {
     todoList.innerHTML = ''
-    tasks.forEach(element => {
-        const liElement = document.createElement('li')
-              liElement.className = 'todo__item'
-              liElement.id = `todo__item__${element.number}`
-              liElement.insertAdjacentHTML('afterbegin', `
-                          <span class="number"> ${element.number}</span>
-                          <span class="text">${element.text}</span>
-                          <span class="tag"> ${element.tag}</span>
-                          <span class="date">${element.date}</span>
-                          <button class="delete__btn" id="delete__btn">Удалить</button> 
-            `)
-        todoList.append(liElement)
-    });
+    if(tasks.length > 0){
+        tasks.forEach((item, index) => {
+            todoList.innerHTML += creatorElement(tasks, index)
+        })
+    }
 }
 
 
@@ -76,3 +102,12 @@ const setDate = () => {
     let dateNow = `${dd}-${mm}-${yyyy} ` 
     return dateNow
 }
+
+const addLocal = () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+}
+
+
+const deleteTask = index => {
+    console.log(index)
+} 
